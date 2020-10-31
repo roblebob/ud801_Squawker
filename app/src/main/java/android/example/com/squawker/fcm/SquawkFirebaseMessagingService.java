bfff6b040cb249744b8191c90f0ebf8f1be480a5
roblebob <robert.koppisch@gmail.com>
2020-10-31 17:20:11 +0100

    ...

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
@@ -2,9 +2,12 @@ package android.example.com.squawker.fcm;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.example.com.squawker.AppExecutors;
 import android.example.com.squawker.R;
+import android.example.com.squawker.provider.SquawkContract;
+import android.example.com.squawker.provider.SquawkProvider;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.util.Log;
@@ -27,6 +30,9 @@ import java.util.concurrent.Executors;
 
 public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
 
+
+
+
     private static final String TAG = SquawkFirebaseMessagingService.class.getSimpleName();
 
     // TODO (2) As part of the new Service - Override onMessageReceived.
@@ -40,29 +46,47 @@ public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
     //   date: Ex. 1484358455343
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-
         Log.e(TAG + "::MessageReceived()  " , "From:  " + remoteMessage.getFrom());
-
 
         // TODO (3) As part of the new Service -
         //   If there is message data, get the data using the keys and do two things with it :
         Map<String, String > data = remoteMessage.getData();
+        if (data.isEmpty()) {
+            super.onMessageReceived(remoteMessage);
+            return;
+        }
 
-        //      1. Display a notification with the first 30 character of the message
-        //      2. Use the content provider to insert a new message into the local database
-        //     Hint: You shouldn't be doing content provider operations on the main thread.
-        //           If you don't know how to make notifications or interact with a content provider
-        //           look at the notes in the classroom for help.
-
-
-        String author = data.get("author");;
-        String authorKey = data.get("authorKey");;
-        String message = data.get("message");;
-        Date date = Date.valueOf(data.get("date"));
+        // TODO (3.1) ... display a notification with the first 30 character of the message
 
 
 
-        super.onMessageReceived(remoteMessage);
+
+
+
+
+        // TODO (3.2) ... use the content provider to insert a new message into the local database
+        //          Hint:   You shouldn't be doing content provider operations on the main thread.
+        //                  If you don't know how to make notifications or interact with a content provider
+        //                  look at the notes in the classroom for help.
+        Executors .newSingleThreadExecutor() .execute(  () -> {
+
+            ContentValues newMessage = new ContentValues();
+
+            newMessage.put( SquawkContract.COLUMN_AUTHOR,
+                    data.get( SquawkContract.COLUMN_AUTHOR));
+
+            newMessage.put( SquawkContract.COLUMN_MESSAGE,
+                    data.get( SquawkContract.COLUMN_MESSAGE).substring(30) );
+
+            newMessage.put( SquawkContract.COLUMN_DATE,
+                    data.get( SquawkContract.COLUMN_DATE));
+
+            newMessage.put( SquawkContract.COLUMN_AUTHOR_KEY,
+                    data.get( SquawkContract.COLUMN_AUTHOR_KEY));
+
+            getContentResolver().insert(SquawkProvider.SquawkMessages.CONTENT_URI, newMessage);
+
+        });
     }
 
 
@@ -70,11 +94,7 @@ public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
 
     private void insertSquawk(@NonNull Map<String,String>  data) {
 
-        Executors.newSingleThreadExecutor().execute( () -> {
 
-
-
-        });
     }
 
     private void sendNotification(@NonNull Map<String,String>  data) {
@@ -83,4 +103,15 @@ public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
 
     }
 
+
+    private String checkMessageString(String string, final int MAX) {
+
+        if (string.length() > MAX) {
+
+            String string_ = string.substring(0, MAX) + "&#2026";
+
+            return string;
+        }
+        return string;
+    }
 }
